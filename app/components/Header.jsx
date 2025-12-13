2d4b3f4bc8617b6e04ab2a33454108f38a2e2f08
nolantuttle <nolantuttle@gmail.com>
2025-12-12 22:12:40 -0700

    Deployment automation test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,8 +11,7 @@ const Header = () => {
                     className='rounded-full w-32' />
             </div>
             <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
-                Hi! I'm Nolan Tuttle! <Image src={assets.hand_icon} alt=''
-                    className='w-6' /></h3>
+                Hi! I'm Nolan Tuttle! </h3>
             <h1 className={`${ovo.className}text-3xl sm:text-6xl lg:text-[66px]`}>
                 Embedded Systems Engineer based in Phoenix </h1>
             <p className='max-w-2xl mx-auto font-Ovo'>
