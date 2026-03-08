fc6ec402a782205e5f663281bf9c725058210a2f
nolantuttle <nolantuttle@gmail.com>
2026-03-07 19:44:45 -0700

    Remove Instagram link

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => {
             border-gray-400 mx-[10%] mt-12 py-6'>
                 <p>© 2026 Nolan Tuttle. All rights reserved</p>
                 <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-                    <li><a target='_blank' href="https://instagram.com/nolantuttle">Instagram</a></li>
                     <li><a target='_blank' href="https://www.linkedin.com/in/nolan-tuttle">LinkedIn</a></li>
                     <li><a target='_blank' href="https://github.com/nolantuttle">Github</a></li>
                 </ul>
